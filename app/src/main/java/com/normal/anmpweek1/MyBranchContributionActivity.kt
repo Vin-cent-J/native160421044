2c2da3aa08fe00e28a081b01ce41ce04aad36fff
hidari-nut <madevis02@gmail.com>
2024-02-22 13:19:46 +0700

    Added my contribution branch

---
+++ app/src/main/java/com/normal/anmpweek1/MyBranchContributionActivity.kt
@@ -0,0 +1,11 @@
+package com.normal.anmpweek1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyBranchContributionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_branch_contribution)
+    }
+}
